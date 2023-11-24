ef3b4b38bc097e344406bb8f5f7ef479f9b7575f
Hephzibah <hephzibahijeje89@gmail.com>
2023-11-24 12:41:49 -0800

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,8 @@
-# Using Puppet that install flask from pip3
+#!/usr/bin/env bash
+# Puppet script that install flask
 
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
