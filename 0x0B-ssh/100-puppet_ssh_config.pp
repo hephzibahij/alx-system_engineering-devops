d97c2f595a60a9233de9fcacb5511f1f57cf8576
Hephzibah <hephzibahijeje89@gmail.com>
2023-11-28 11:49:16 -0800

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,27 +1,40 @@
-#!/usr/bin/env bash
-# Puppet script to change client config file ssh
+# using puppet to make changes to the default ssh config file
+# so that one can connect to a server without typing a password.
 
-file_line { 'Turn off passwd auth':
-  path  => '/etc/ssh/sshd_config',
-  line  => 'PasswordAuthentication no',
-  match => '^#PasswordAuthentication yes',
+include stdlib
+
+file_line { 'SSH Private Key':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    IdentityFile ~/.ssh/school',
+  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
+  replace            => true,
+  append_on_no_match => true
 }
 
-file_line { 'Declare identity file':
-  path  => '/etc/ssh/ssh_config',
-  line  => 'IdentityFile ~/.ssh/id_rsa',
-  match => '^#   IdentityFile ~/.ssh/id_rsa',
+# Regex match explanation
+#
+# ^       beginning of the line
+# [#]*  atleast one hash character
+# [\s]*  zero or more white space characters
+# (?i)IdentityFile case insensitive "IdentityFile"
+# [\s]+ at least one whitespace character
+# ~/.ssh/id_rsa The ssh private key file path we want to replace
+# $      end of the line
+
+file_line { 'Deny Password Auth':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    PasswordAuthentication no',
+  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
+  replace            => true,
+  append_on_no_match => true
 }
 
-file { '/home/vagrant/.ssh/id_rsa':
-  source  => '/vagrant/id_rsa',
-  mode    => '0600',
-  owner   => 'vagrant',
-  group   => 'vagrant',
-}
-
-class { 'ssh':
-  service_hasstatus => false,
-  service_restart   => '/etc/init.d/ssh reload',
-  service_name      => 'ssh',
-}
+# Regex match explanation
+#
+# ^       beginning of the line
+# [#]*  atleast one hash character
+# [\s]*  zero or more white space characters
+# (?i)PasswordAuthentication case insensitive "PasswordAuthentication"
+# [\s]+ at least one whitespace character
+# (yes|no) with the value "yes" or the value "no"
+# $      end of the line
